bc1aa298ae49bcb4d2ddec78619454e6b6508a20
LukasGuzys <lukas.guzys@gmail.com>
2022-02-16 11:11:23 +0200

    inputs to console at page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+
+  const [clientName, enterClientName] = useState("")
+  const [clientSurname, enterClientSurname] = useState("")
+  const [clientEmail, enterClientEmail] = useState("")
+  const [clientAge, enterClientAge] = useState(0)
+
+  const addToList = () => {
+    console.log(clientName + clientSurname + clientEmail + clientAge);
+  }
+
   return (
     <div className="App">
       <h1>Clients List</h1>
 
       <label>Name: </label>
-      <input type="text"/>
+      <input type="text" onChange={(event) =>{enterClientName(event.target.value)}}/>
       <label>Surname: </label>
-      <input type="text"/>
+      <input type="text"onChange={(event) =>{enterClientSurname(event.target.value)}}/>
       <label>Email: </label>
-      <input type="email"/>
+      <input type="text"onChange={(event) =>{enterClientEmail(event.target.value)}}/>
       <label>Age: </label>
-      <input type="number"/>
-      <button>Add Client</button>
+      <input type="number" onChange={(event) =>{enterClientAge(event.target.value)}}/>
+      <button onClick={addToList}>Add Client</button>
     </div>
   );
 }
